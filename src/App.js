eba9391c61e0365fe1eed4c7d6e787292950bf1f
Taffie Theuri <taffietheuri4@gmail.com>
2023-01-25 23:24:44 +0300

    Create Remaining component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from "./components/Budget";
+import Remaining from "./components/Remaining";
 
 
 function App() {
@@ -11,7 +12,12 @@ function App() {
   <div className="col-sm">
     <Budget/>
   </div>
+  <div className="col-sm">
+    <Remaining/>
+  </div>
 </div>
+
+
     </div>
   )
 }
---
+++ src/components/Remaining.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Remaining() {
+  return (
+    <div className='alert alert-success'>
+        <span>Remaining:</span>
+
+
+    </div>
+  )
+}
+
+export default Remaining
