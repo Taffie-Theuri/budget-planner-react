e9df5956ba051fb5044b288591dd3398b73081e6
Taffie Theuri <taffietheuri4@gmail.com>
2023-01-26 00:18:18 +0300

    Complete Budget Component

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState, useContext } from 'react';
+import ViewBudget from './ViewBudget';
+import EditBudget from './EditBudget';
+import { AppContext } from '../context/AppContext';
 
-function Budget() {
-  return (
-    <div className='alert alert-secondary'>
-        <span>Budget:</span>
+const Budget = () => {
+	const { budget, dispatch } = useContext(AppContext);
+	const [isEditing, setIsEditing] = useState(false);
 
+	const handleEditClick = () => {
+		setIsEditing(true);
+	};
 
-    </div>
-  )
-}
+	const handleSaveClick = (value) => {
+		dispatch({
+			type: 'SET_BUDGET',
+			payload: value,
+		});
+		setIsEditing(false);
+	};
 
-export default Budget
+	return (
+		<div class='alert alert-secondary p-3 d-flex align-items-center justify-content-between'>
+			{isEditing ? (
+				<EditBudget handleSaveClick={handleSaveClick} budget={budget} />
+			) : (
+				<ViewBudget handleEditClick={handleEditClick} budget={budget} />
+			)}
+		</div>
+	);
+};
+
+export default Budget;
