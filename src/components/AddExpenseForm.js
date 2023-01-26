afdcc08f502b8744bcfa140459df7816c6504f81
mknyambura <kariuki.mercie@gmail.com>
2023-01-26 07:25:00 +0300

    update spent so far form

--- src/components/AddExpenseForm.js
+++ src/components/AddExpenseForm.js
@@ -9,6 +9,8 @@ function AddExpenseForm() {
 
 const onSubmit = (event) => {
 	event.preventDefault();
+	alert('name' + name + 'cost' + cost)
+
 	const expense = {
 
 		id: uuidv4(),
@@ -23,38 +25,38 @@ const onSubmit = (event) => {
 };
 
   return (
-    <form  onSubmit= {onSubmit}>
+    <form onSubmit= {onSubmit}>
         <div class='row'>
-				<div class='col-sm col-lg-4'>
-					<label for='name'>Name</label>
-					<input
-						required='required'
-						type='text'
-						class='form-control'
-						id='name'
-						 value={name}
-						onChange={(event) => setName(event.target.value)}
-					/>
-				</div>
-				<div class='col-sm col-lg-4'>
-					<label for='cost'>Cost</label>
-					<input
-						required='required'
-						type='number'
-						class='form-control'
-						id='cost'
-						value={cost}
-						onChange={(event) => setCost(event.target.value)}
-					/>
-				</div>
+			<div class='col-sm col-lg-4'>
+				<label for='name'>Name</label>
+				<input
+					required='required'
+					type='text'
+					class='form-control'
+					id='name'
+					 value={name}
+					onChange={(event) => setName(event.target.value)}
+				/>
 			</div>
-			<div class='row mt-3'>
-				<div class='col-sm'>
-					<button type='submit' class='btn btn-primary'>
-						Save
-					</button>
-				</div>
+			<div class='col-sm col-lg-4'>
+				<label for='cost'>Cost</label>
+				<input
+					required='required'
+					type='number'
+					class='form-control'
+					id='cost'
+					value={cost}
+					onChange={(event) => setCost(event.target.value)}
+				/>
 			</div>
+		</div>
+		<div class='row mt-3'>
+			<div class='col-sm'>
+				<button type='submit' class='btn btn-primary'>
+					Save
+				</button>
+			</div>
+		</div>
     </form>
   )
 }
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,26 +5,26 @@ import App from './App';
 
 
 const ExpenseList = () => {
-     const expenses = [
-      { id: 435626, name: "Transport", cost: 1000},
-      { id: 435626, name: "Rent", cost: 5000},
-      { id: 435626, name: "Upkeep", cost: 2500},
-      { id: 435626, name: "Entertainment", cost: 3000},
-      { id: 435626, name: "Shopping", cost: 4000},
-    ] 
+  const expenses = [
+    { id: 435626, name: "Transport", cost: 1000},
+    { id: 435626, name: "Rent", cost: 5000},
+    { id: 435626, name: "Upkeep", cost: 2500},
+    { id: 435626, name: "Entertainment", cost: 3000},
+    { id: 435626, name: "Shopping", cost: 4000},
+  ] 
   
   return (
     
-        <ul
-            className= 'list-group'>
-              {expenses.map((expense) =>(
-                <ExpenseItem
-                  id={expense.id}
-                  name={expense.name}
-                  cost={expense.cost}/>
-                
-              ))}
-        </ul>
+    <ul
+      className= 'list-group'>
+        {expenses.map((expense) =>(
+          <ExpenseItem
+            id={expense.id}
+            name={expense.name}
+            cost={expense.cost}/>
+          
+        ))}
+    </ul>
 
     
   );
